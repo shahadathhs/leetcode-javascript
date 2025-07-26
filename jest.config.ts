a37cf49dcdc9d5e04e2e72d69c0961a5ae8410b7
shahadathhs <shahadathhossensajib732@gmail.com>
2025-07-26 18:37:59 +0600

    fix: fixed lint error

--- jest.config.ts
+++ jest.config.ts
@@ -1,23 +1,24 @@
-import { pathsToModuleNameMapper, type JestConfigWithTsJest } from 'ts-jest';
-import { readFileSync } from 'fs';
-import path from 'path';
+import { pathsToModuleNameMapper, type JestConfigWithTsJest } from 'ts-jest'
+import { readFileSync } from 'fs'
+import path from 'path'
 
-const tsconfig = JSON.parse(
-  readFileSync(path.join(__dirname, 'tsconfig.json'), 'utf-8')
-);
+const tsconfig = JSON.parse(readFileSync(path.join(__dirname, 'tsconfig.json'), 'utf-8'))
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: 'tsconfig.json',
-    }],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
   moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
     prefix: '<rootDir>/src/',
   }),
-};
+}
 
-export default config;
+export default config
