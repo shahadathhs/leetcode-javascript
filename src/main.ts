059cdc8943b00cda1f22086b1caf47c5e02b7ede
shahadathhs <shahadathhossensajib732@gmail.com>
2025-07-26 18:46:05 +0600

    fix: fixed build error

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import { checkIfInstanceOf } from '@/app/2618-check-if-object-instance-of-class'
+
 function main() {
   console.log('Hello, World!')
+  const dateInstance = new Date()
+  console.log(checkIfInstanceOf(dateInstance, Date)) // true
 }
 
 main()
