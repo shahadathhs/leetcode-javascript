92bc8e89e5f1cfeb9f7288fcc113d35a557076dc
shahadathhs <shahadathhossensajib732@gmail.com>
2025-07-26 18:01:52 +0600

    chore: fixed test configuration warning

--- jest.config.ts
+++ jest.config.ts
@@ -1,22 +1,23 @@
-import type { JestConfigWithTsJest } from 'ts-jest'
-import { pathsToModuleNameMapper } from 'ts-jest'
-import { readFileSync } from 'fs'
-import path from 'path'
+import { pathsToModuleNameMapper, type JestConfigWithTsJest } from 'ts-jest';
+import { readFileSync } from 'fs';
+import path from 'path';
 
-const tsconfig = JSON.parse(readFileSync(path.join(__dirname, 'tsconfig.json'), 'utf-8'))
+const tsconfig = JSON.parse(
+  readFileSync(path.join(__dirname, 'tsconfig.json'), 'utf-8')
+);
 
 const config: JestConfigWithTsJest = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+    }],
+  },
   moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
     prefix: '<rootDir>/src/',
   }),
-  globals: {
-    'ts-jest': {
-      isolatedModules: true,
-    },
-  },
-}
+};
 
-export default config
+export default config;
